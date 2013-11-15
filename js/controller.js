d311c82b7d6b349ffb7f1eabd5293e492d4eaa49
unknown <iguest@IS-M334L23-32.ischool.uw.edu>
2013-11-15 12:20:46 -0800

    Almost finished.

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,34 @@
 */
 
 $(function(){
+	var formatLabels = {
+		dvd: 'DVD',
+		bluray: 'Blu-Ray'
+	};
 
+	var cartModel = createCartModel();
+
+	var cartView = createCartView({
+		model: cartModel,
+		template: $('.cart-item-template'),
+		container: $('.cart-items-container'),
+		totalPrice: $('.total-price')
+	});
+
+	var moviesModel = createMoviesModel({
+		url: 'https://courses.washington.edu/info343/ajax/movies/'
+	});
+
+	var moviesView = createMoviesView({
+		model: moviesModel,
+		template: $('.movie-template'),
+		container: $('.movies-container')
+	});
+
+	moviesModel.refresh();
+
+	moviesView.on('addToCart', function(data){
+
+	});
 }); //doc ready()
 
